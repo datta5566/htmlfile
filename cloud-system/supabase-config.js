6fcc5188f502bf188c402e5fd01f644dfd9395cf
mr___Dk <kale63633@gmail.com>
2026-07-10 12:26:15 +0530

    Add cloud configuration template

---
+++ cloud-system/supabase-config.js
@@ -0,0 +1,7 @@
+window.DK_CLOUD_CONFIG = Object.freeze({
+  // Supabase Dashboard > Project Settings > Data API
+  supabaseUrl: "PASTE_YOUR_SUPABASE_PROJECT_URL",
+  supabasePublishableKey: "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY",
+  storageBucket: "dk-app-files",
+  syncIntervalMs: 15000
+});
