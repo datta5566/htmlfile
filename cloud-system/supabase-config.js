b20d95acbc5a5a53a11a4d5d0ef1ead701f40db6
mr___Dk <kale63633@gmail.com>
2026-07-10 13:55:12 +0530

    Save Supabase project URL

--- cloud-system/supabase-config.js
+++ cloud-system/supabase-config.js
@@ -1,6 +1,6 @@
 window.DK_CLOUD_CONFIG = Object.freeze({
   // Supabase Dashboard > Project Settings > Data API
-  supabaseUrl: "PASTE_YOUR_SUPABASE_PROJECT_URL",
+  supabaseUrl: "https://xcmkyltojcmeowxbtsfi.supabase.co",
   supabasePublishableKey: "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY",
   storageBucket: "dk-app-files",
   syncIntervalMs: 15000
